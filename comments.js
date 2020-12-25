b88e39df8cf0b3da99b9c7a3407dac80903f7c51
Lotuslinny <linny@fegg.nl>
2020-12-25 14:37:55 +0100

    Test Farm Done!

---
+++ comments.js
@@ -0,0 +1,8 @@
+//you can not use different files, because the first four functions must be in farm.js, the code that musn't be changed demands it.
+// you can not use decimals here for the evironmentfactors. http://adripofjavascript.com/blog/drips/avoiding-problems-with-decimal-math-in-javascript.html
+//The typeof operator returns a string indicating the type of the unevaluated operand.
+//for - loops through a block of code a number of times ***for (var prop in o.sun)***
+//variable
+//A different property name is assigned to variable on each iteration.
+//object
+//Object whose non-Symbol enumerable properties are iterated over.
